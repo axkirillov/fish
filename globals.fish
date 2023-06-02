ded8a6ff3b9fae8c69038cdcef25c4e87facd841
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-06-02 12:42:20 +0200

    bind c-j to enter in fzf

--- globals.fish
+++ globals.fish
@@ -5,6 +5,7 @@ set -xg NVIM_CONFIG $CONFIG/nvim
 set -xg EDITOR nvim
 set -xg VISUAL $EDITOR
 set -xg FZF_DEFAULT_COMMAND "fd --type f -H"
+set -xg FZF_DEFAULT_OPTS "--bind=ctrl-j:accept"
 # needed for ruby integration with neovim
 set -xg GEM_HOME $HOME/.gem/bin/
 
