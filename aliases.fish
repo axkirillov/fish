364fa9b776cf5bfad2e607e6b7d2578dc05dc131
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-06-05 11:52:48 +0200

    cleanup some aliases

--- aliases.fish
+++ aliases.fish
@@ -1,6 +1,5 @@
 # aliases
 alias 9=k9s
-alias delete-released-branches="git branch --merged | egrep -v '(^\*|master|develop)' | xargs git branch -d"
 alias k="kubectl"
 alias ls="lsd"
 alias ssh-add-rsa="ssh-add $HOME/.ssh/id_rsa"
@@ -12,11 +11,9 @@ alias rm="rm -i"
 alias request="make -C $HOME/requests/"
 alias requests="vi $HOME/requests/Makefile" 
 alias repo "cd $HOME/repo/(ls $HOME/repo | fzf)"
-alias f "floaterm"
 alias lzd="lazydocker"
 alias lzg="lazygit"
 alias run-branch="gh workflow run build.yaml --ref (git rev-parse --abbrev-ref HEAD)"
-alias ring-bell-3-times="tput bel && sleep 0.1 && tput bel && sleep 0.1 && tput bel"
 alias lg="$HOME/scripts/fzf-grep.sh"
 alias changed-files="git diff --name-only (git merge-base HEAD develop)"
 alias add-ticket-number="~/repo/bulk-edit-git-commit-messages/begcm.sh"
