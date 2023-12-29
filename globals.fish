ee7f100e24c69168c48fc4f4ebe347f6c9d6dca6
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-12-29 15:27:58 +0100

    rm fzf congig

--- globals.fish
+++ globals.fish
@@ -2,7 +2,5 @@
 set -xg XDG_CONFIG_HOME "$HOME/.config"
 set -xg EDITOR nvim
 set -xg VISUAL $EDITOR
-set -xg FZF_DEFAULT_COMMAND "fd --type f -H"
-set -xg FZF_DEFAULT_OPTS "--bind=ctrl-j:accept"
 # needed for ruby integration with neovim
 set -xg GEM_HOME $HOME/.gem/bin/
