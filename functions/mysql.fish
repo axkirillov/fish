eee57ea7c4040a25f5f60853a998f1a20060ce3f
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-01-08 13:40:17 +0100

    update mysql function

--- functions/mysql.fish
+++ functions/mysql.fish
@@ -1,17 +1,21 @@
 function mysql
     set -l host
 	set -l port 3306
+	set -l user root
+	set -l password toor
     switch $argv
-        case local
-            set host minikube.local
         case remote
             set host localhost    
 		case docker 
 			set host localhost
 			set port 3360
+		case prod
+			set host localhost
+			set user myposter
+			set password $MYSQL_PASSWORD
     end
     echo opening connection...
-    mycli -u root -h $host myposter -p toor -P $port
+    mycli -u $user -h $host myposter -p $password -P $port
 end
 
 function mysql-remote-log
