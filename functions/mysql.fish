394972e5bc1c538a27b015e1ff21560e7d0c0948
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-01-12 17:39:47 +0100

    update mysql function

--- functions/mysql.fish
+++ functions/mysql.fish
@@ -3,22 +3,27 @@ function mysql
 	set -l port 3306
 	set -l user root
 	set -l password toor
-    switch $argv
+	set -l host localhost    
+	set -l db myposter
+    switch $argv[1]
         case remote
-            set host localhost    
-		case docker 
-			set host localhost
-			set port 3360
 		case prod
-			set host localhost
 			set user myposter
 			set password $MYSQL_PASSWORD
-    end
+		case jq
+			set user root_remote
+			set db juniqe
+			set password toortoor
+			switch $argv[2]
+				case prod
+					set password "*Nl8XLiKF6x4^22O"
+			end
+	end
     echo opening connection...
-    mycli -u $user -h $host myposter -p $password -P $port
+    mycli -u $user -h $host $db -p $password -P $port
 end
 
 function mysql-remote-log
-    mycli -u root -h localhost myposter -p toor -P 3306 -l log.txt
+    mycli -u root -h localhost $db -p toor -P 3306 -l log.txt
 end
 
