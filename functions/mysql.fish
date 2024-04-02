1fdae783d36bbfdd119d856213ea2ff927a3fff8
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-04-02 13:03:10 +0200

    update mysql

--- functions/mysql.fish
+++ functions/mysql.fish
@@ -1,12 +1,13 @@
 function mysql
-    set -l host
 	set -l port 3306
 	set -l user root
 	set -l password toor
 	set -l host localhost    
 	set -l db myposter
+	set -l log false
     switch $argv[1]
         case remote
+			echo connecting... user: $user, host: $host
 		case prod
 			set user myposter
 			set password $MYSQL_PASSWORD
@@ -18,12 +19,14 @@ function mysql
 				case prod
 					set password "*Nl8XLiKF6x4^22O"
 			end
+	case monetary
+		set password root
+		set db monetary
+		set port 10120
+	end
+	if test $argv[2] = log
+		set logarg = "-l log.txt"
 	end
     echo opening connection...
-    mycli -u $user -h $host $db -p $password -P $port
+    mycli -u $user -h $host $db -p $password -P $port $logarg
 end
-
-function mysql-remote-log
-    mycli -u root -h localhost $db -p toor -P 3306 -l log.txt
-end
-
