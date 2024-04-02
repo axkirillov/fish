e7b1185007e5d9deae90ded671dcad90f9d188c1
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-04-02 13:03:18 +0200

    update aliases

--- aliases.fish
+++ aliases.fish
@@ -6,11 +6,11 @@ alias update-kitty="curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /d
 alias v=nvim
 alias vi=nvim
 alias vim=nvim
-alias rm="rm -i"
 alias lzd="lazydocker"
 alias lg="$HOME/scripts/fzf-grep.sh"
 alias add-ticket-number="~/repo/bulk-edit-git-commit-messages/begcm.sh"
 alias mfa ~/repo/dev-cluster/aws/aws-mfa-check.sh
 alias repo "cd $HOME/repo/(ls $HOME/repo | fzf)"
-#close all tmux windows but current
-alias closeall "tmux list-windows -F '#{window_id}' | grep -v $(tmux display-message -p '#{window_id}') | xargs -I {} tmux kill-window -t {}"
+alias hx "hx -c $HOME_MANAGER/helix/config.toml"
+alias v 'fg $(eval "pgrep -P $fish_pid nvim") || nvim'
+alias last-run "open -u $(gh run list --limit 1 --json url --jq '.[0].url')"
