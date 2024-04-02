8e795f8ab0c8bd945e932daea2d937c88ee6de02
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-04-02 13:03:24 +0200

    add home manager global

--- globals.fish
+++ globals.fish
@@ -1,5 +1,6 @@
 # global vars
 set -xg XDG_CONFIG_HOME "$HOME/.config"
+set -xg HOME_MANAGER "$HOME/repo/home-manager"
 set -xg EDITOR nvim
 set -xg VISUAL $EDITOR
 # needed for ruby integration with neovim
